9f77e5dcb0090ae60ded27584766504f13a9421a
lsj0202 <w1004123@gmail.com>
2024-01-09 17:12:15 +0900

    feat: add globalStyle

---
+++ src/app/(beforeLogin)/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import styled from "@emotion/styled";
+
+const Page = () => {
+  return <div>sdf</div>;
+};
+
+export default Page;
+
+const StyledPage = styled.div`
+  background-color: pink;
+`;
---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,21 @@
+import { css } from "@emotion/react";
+
+const GlobalStyle = css`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font: inherit;
+    vertical-align: baseline;
+    text-decoration: none;
+  }
+
+  input::-webkit-search-decoration,
+  input::-webkit-search-cancel-button,
+  input::-webkit-search-results-button,
+  input::-webkit-search-results-decoration {
+    display: none;
+  }
+`;
+
+export default GlobalStyle;
